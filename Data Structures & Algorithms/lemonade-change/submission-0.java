60609a5a982f2645142f54c6eba22b196765798d
Anindya-Roy-Work <anindyaroywork@gmail.com>
2026-05-31 00:47:04 +0530

    Add: lemonade-change - submission-0

---
+++ Data Structures & Algorithms/lemonade-change/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0;
+        int ten = 0;
+        int twenty = 0;
+        for(int bill : bills){
+            if(bill == 5) five++;
+            else if(bill == 10){
+                ten++;
+                if(five == 0) return false;
+                five--;
+            }else{
+                twenty++;
+                if(ten == 0){
+                    if(five < 3) return false;
+                    five-=3;
+                }else{
+                    ten--;
+                    if(five == 0) return false;
+                    five--;
+                }
+            }
+        }
+        return true;
+    }
+}
